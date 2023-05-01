d9806fdf55f1c367b8b702fda346df933325f995
YuriiReshetnyk <yura.reshetnyk295@gmail.com>
2023-05-01 16:47:20 +0300

    lab4 is done

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
-from typing import Optional
 
 
-def create_temporary_array(pattern: str):
-    pattern_len = len(pattern)
-    temporary_array = [0] * len(pattern)
+def find_ways_count(matrix: list, width: int, height: int):
 
-    j = 0
-    i = 1
-    while i < pattern_len:
-        if pattern[j] == pattern[i]:
-            temporary_array[i] = j + 1
-            i += 1
-            j += 1
-        else:
-            if j != 0:
-                j = temporary_array[j - 1]
+    squares = [[] for i in range(width)]
+    for row in range(height):
+        squares[0].append(1)
+
+    memoization = {}
+    for row in range(height):
+        memoization.setdefault(matrix[row][0], 0)
+        memoization[matrix[row][0]] += 1
+
+    for column in range(1, width):
+        ways = {}
+
+        for row in range(height):
+            square = matrix[row][column]
+            memoization.setdefault(square, 0)
+
+            if square is not matrix[row][column-1]:
+                way = squares[column - 1][row] + memoization[square]
             else:
-                temporary_array[i] = 0
-                i += 1
+                way = memoization[square]
 
-    return temporary_array
+            ways[square] = ways.get(square, 0) + way
+            squares[column].append(way)
 
+        for k in ways:
+            memoization[k] += ways[k]
 
-def kmp_search(string: str, pattern: str) -> Optional[int]:
-    string_len = len(string)
-    pattern_len = len(pattern)
-    temporary_array = create_temporary_array(pattern)
-
-    i = 0
-    j = 0
-    while i < string_len:
-
-        if string[i] == pattern[j]:
-            i += 1
-            j += 1
-        else:
-            if j == 0:
-                i += 1
-            else:
-                j = temporary_array[j - 1]
-
-        if j == pattern_len:
-            return i - pattern_len
-
-    return None
+    if height == 1:
+        return squares[width-1][0]
+    return squares[width-1][0] + squares[width-1][height-1]
 
 
 def main():
-    string = input("Enter the string: ")
-    pattern = input("Enter the pattern: ")
-    print(kmp_search(string, pattern))
+    with open('ijones.in', 'r') as ijones:
+        width, height = map(int, ijones.readline().split())
+
+        matrix = []
+        for i in range(height):
+            matrix.append(ijones.readline().rstrip('\n'))
+
+    with open('ijones.out', 'w') as ijones:
+        ijones.write(str(find_ways_count(matrix, width, height)))
 
 
 if __name__ == '__main__':
