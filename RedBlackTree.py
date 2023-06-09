375d97b3ba488776d1360951f3888622dd790595
YuriiReshetnyk <yura.reshetnyk295@gmail.com>
2023-06-09 22:33:51 +0300

    Done lab1

---
+++ RedBlackTree.py
@@ -0,0 +1,166 @@
+from Node import Node
+
+
+class RedBlackTree:
+
+    def __init__(self):
+        self.NULL = Node(0)
+        self.NULL.color = 0
+        self.root = self.NULL
+
+    def __minimum(self, node):
+        while node.left != self.NULL:
+            node = node.left
+        return node
+
+    def __left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.NULL:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def __right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.NULL:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def __fix_delete(self, x):
+        while x != self.root and x.color == 0:
+            if x == x.parent.left:
+                s = x.parent.right
+                if s.color == 1:
+                    s.color = 0
+                    x.parent.color = 1
+                    self.__left_rotate(x.parent)
+                    s = x.parent.right
+                if s.left.color == 0 and s.right.color == 0:
+                    s.color = 1
+                    x = x.parent
+                else:
+                    if s.right.color == 0:
+                        s.left.color = 0
+                        s.color = 1
+                        self.__right_rotate(s)
+                        s = x.parent.right
+
+                    s.color = x.parent.color
+                    x.parent.color = 0
+                    s.right.color = 0
+                    self.__left_rotate(x.parent)
+                    x = self.root
+            else:
+                s = x.parent.left
+                if s.color == 1:
+                    s.color = 0
+                    x.parent.color = 1
+                    self.__right_rotate(x.parent)
+                    s = x.parent.left
+
+                if s.right.color == 0 and s.right.color == 0:
+                    s.color = 1
+                    x = x.parent
+                else:
+                    if s.left.color == 0:
+                        s.right.color = 0
+                        s.color = 1
+                        self.__left_rotate(s)
+                        s = x.parent.left
+
+                    s.color = x.parent.color
+                    x.parent.color = 0
+                    s.left.color = 0
+                    self.__right_rotate(x.parent)
+                    x = self.root
+        x.color = 0
+
+    def __rb_transplant(self, u, v):
+        if u.parent is None:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        v.parent = u.parent
+
+    def delete_node_helper(self, node, key):
+        z = self.NULL
+        while node != self.NULL:
+            if node.val == key:
+                z = node
+
+            if node.val <= key:
+                node = node.right
+            else:
+                node = node.left
+
+        if z == self.NULL:
+            print("Value not present in Tree !!")
+            return
+
+        y = z
+        y_original_color = y.color
+        if z.left == self.NULL:
+            x = z.right
+            self.__rb_transplant(z, z.right)
+        elif z.right == self.NULL:
+            x = z.left
+            self.__rb_transplant(z, z.left)
+        else:
+            y = self.__minimum(z.right)
+            y_original_color = y.color
+            x = y.right
+            if y.parent == z:
+                x.parent = y
+            else:
+                self.__rb_transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+
+            self.__rb_transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        if y_original_color == 0:
+            self.__fix_delete(x)
+
+    def delete_node(self, val):
+        self.delete_node_helper(self.root, val)
+
+    def __printCall(self, node, indent, last):
+        if node != self.NULL:
+            print(indent, end=' ')
+            if last:
+                print("R----", end=' ')
+                indent += "     "
+            else:
+                print("L----", end=' ')
+                indent += "|    "
+
+            s_color = "RED" if node.color == 1 else "BLACK"
+            print(str(node.val) + "(" + s_color + ")")
+            self.__printCall(node.left, indent, False)
+            self.__printCall(node.right, indent, True)
+
+    def print_tree(self):
+        self.__printCall(self.root, "", True)
